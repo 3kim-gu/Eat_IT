bd17cf40a2bea94f375445044c9dd33e05fead59
dyoun12 <gdaeyeon002@gmail.com>
2025-01-10 12:48:06 +0900

    Update RestaurantController.java

--- src/restaurant/controller/RestaurantController.java
+++ src/restaurant/controller/RestaurantController.java
@@ -2,7 +2,28 @@ package restaurant.controller;
 
 public class RestaurantController {
 
-	public static void getRestaurantByCategory(String string) {
+
+	public static void selectAllRestaurant() {
+		
+	}
+
+	public static void selectRestaurantByCategory(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void selectRestaurantByPrice(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void selectRestaurantByDistance(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void selectRestaurantByRname(String string) {
+		// TODO Auto-generated method stub
 		
 	}
 
