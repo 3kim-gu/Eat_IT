f3ffed29ae1c297ee666e8fab830c82559b6e0be
minsung159357 <minsung159357@gmail.com>
2025-01-10 16:10:21 +0900

    DAO 수정3

--- src/restaurant/model/RestaurantDAO.java
+++ src/restaurant/model/RestaurantDAO.java
@@ -15,7 +15,10 @@ public class RestaurantDAO {
 	@Test
 	public void Test()	{
 		try {
-			ArrayList<RestaurantDTO> rd = RestaurantDAO.selectRestaurantByCategory("양식");
+//			ArrayList<RestaurantDTO> rd = RestaurantDAO.selectRestaurantByCategory("양식");
+//			ArrayList<RestaurantDTO> rd = RestaurantDAO.selectRestaurantByPrice(5000, 10000);
+//			ArrayList<RestaurantDTO> rd = selectRestaurantByDistance(5);
+			RestaurantDTO rd = selectRestaurantByRname("이");
 			System.out.println(rd);
 		} catch (SQLException e) {
 			System.out.println("예외처리오류");
@@ -24,41 +27,135 @@ public class RestaurantDAO {
 	}
 //	1. 메뉴 카테고리로 음식점 검색
 	public static ArrayList<RestaurantDTO> selectRestaurantByCategory(String category) throws SQLException {
-		Connection con = null;
+		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rset = null;
+		ResultSet rs = null;
 		ArrayList<RestaurantDTO> restaurant = new ArrayList<>();
 
 		try {
-			con = DBUtil.getConnection();
-			pstmt = con.prepareStatement("select * from restaurant where category=?");
+			conn = DBUtil.getConnection();
+			pstmt = conn.prepareStatement("select * from restaurant where category=?");
 			pstmt.setString(1, category);
-			rset = pstmt.executeQuery();
-			while (rset.next()) {
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
 				restaurant.add(
 						RestaurantDTO.builder()
-						.rname(rset.getString(1))
-						.category(rset.getString(2))
-						.food(rset.getString(3))
-						.price(rset.getInt(4))
-						.distance(rset.getInt(5))
-						.waiting_time(rset.getInt(6))
-						.is_able_group(rset.getString(7))
-						.score(rset.getInt(8))
-						.review(rset.getString(9))
-						.url(rset.getString(10))
+						.rname(rs.getString(1))
+						.category(rs.getString(2))
+						.food(rs.getString(3))
+						.price(rs.getInt(4))
+						.distance(rs.getInt(5))
+						.waiting_time(rs.getInt(6))
+						.is_able_group(rs.getString(7))
+						.score(rs.getInt(8))
+						.review(rs.getString(9))
+						.url(rs.getString(10))
 						.build()
 				);
 			}
 		} finally {
-			DBUtil.close(con, pstmt, rset);
+			DBUtil.close(conn, pstmt, rs);
 		}
 		return restaurant;
 	}
 //	2. 가격 범위로 음식점 검색
-	public static selectRestaurantByPrice
+	public static ArrayList<RestaurantDTO> selectRestaurantByPrice(int row, int high) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RestaurantDTO> restaurant = new ArrayList<>();
+
+		try {
+			conn = DBUtil.getConnection();
+			pstmt = conn.prepareStatement("select * from restaurant where price >= ? and price <= ?");
+			pstmt.setInt(1, row);
+			pstmt.setInt(2, high);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				restaurant.add(
+						RestaurantDTO.builder()
+						.rname(rs.getString(1))
+						.category(rs.getString(2))
+						.food(rs.getString(3))
+						.price(rs.getInt(4))
+						.distance(rs.getInt(5))
+						.waiting_time(rs.getInt(6))
+						.is_able_group(rs.getString(7))
+						.score(rs.getInt(8))
+						.review(rs.getString(9))
+						.url(rs.getString(10))
+						.build()
+				);
+			}
+		} finally {
+			DBUtil.close(conn, pstmt, rs);
+		}
+		return restaurant;
+	}
 //	3. 거리순으로 음식점 검색
+	public static ArrayList<RestaurantDTO> selectRestaurantByDistance(int min) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<RestaurantDTO> restaurant = new ArrayList<>();
+
+		try {
+			conn = DBUtil.getConnection();
+			pstmt = conn.prepareStatement("select * from restaurant where distance <= ?");
+			pstmt.setInt(1, min);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				restaurant.add(
+						RestaurantDTO.builder()
+						.rname(rs.getString(1))
+						.category(rs.getString(2))
+						.food(rs.getString(3))
+						.price(rs.getInt(4))
+						.distance(rs.getInt(5))
+						.waiting_time(rs.getInt(6))
+						.is_able_group(rs.getString(7))
+						.score(rs.getInt(8))
+						.review(rs.getString(9))
+						.url(rs.getString(10))
+						.build()
+				);
+			}
+		} finally {
+			DBUtil.close(conn, pstmt, rs);
+		}
+		return restaurant;
+	}
 //	4. 음식점 이름으로 음식점 검색
+	public static RestaurantDTO selectRestaurantByRname(String rname) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		RestaurantDTO restaurant = null;
+
+		try {
+			conn = DBUtil.getConnection();
+			pstmt = conn.prepareStatement("select * from restaurant where rname=?");
+			pstmt.setString(1, rname);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				restaurant = RestaurantDTO.builder()
+						.rname(rs.getString(1))
+						.category(rs.getString(2))
+						.food(rs.getString(3))
+						.price(rs.getInt(4))
+						.distance(rs.getInt(5))
+						.waiting_time(rs.getInt(6))
+						.is_able_group(rs.getString(7))
+						.score(rs.getFloat(8))
+						.review(rs.getString(9))
+						.url(rs.getString(10))
+						.build();
+			}
+		} finally {
+			DBUtil.close(conn, pstmt, rs);
+		}
+		return restaurant;
+	}
 //	5. 가격, 거리 기준으로 음식점 검색
 //	6. 카테고리, 가격 기준으로 음식점 검색
 //	7. 전체검색
