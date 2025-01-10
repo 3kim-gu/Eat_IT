3bd233d691dc427412e82f9403d40e0f3900ddef
김리영 (Kim Ri-yeong) <fldud6522@gmail.com>
2025-01-10 12:43:26 +0900

    DTO

--- src/restaurant/domain/RestaurantDTO.java
+++ src/restaurant/domain/RestaurantDTO.java
@@ -1,5 +1,24 @@
 package restaurant.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class RestaurantDTO {
-	
+	private String rname;
+	private String category;
+	private String food;
+	private int price;
+	private int distance;
+	private int watting_time;
+	private String is_able_group;
+	private float score;
+	private String review;
+	private String url;
 }
