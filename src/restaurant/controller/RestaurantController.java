55d82d7af7266572c76653372f70a66b8a04304f
dyoun12 <gdaeyeon002@gmail.com>
2025-01-10 15:00:03 +0900

    <RestuarantCountroller.java> model, view 메소드 연결

--- src/restaurant/controller/RestaurantController.java
+++ src/restaurant/controller/RestaurantController.java
@@ -1,30 +1,102 @@
 package restaurant.controller;
 
+import java.sql.SQLException;
+
+import restaurant.domain.RestaurantDTO;
+import restaurant.model.RestaurantDAO;
+import restaurant.view.RunningEndView;
+
 public class RestaurantController {
 
-
 	public static void selectAllRestaurant() {
-		
+		try {
+			RunningEndView.printResult(RestaurantDAO.selectAllRestaurant());
+		} catch (SQLException e) {
+			System.out.println("selectAllRestaurant 검색 불가.");
+			e.printStackTrace();
+		}
 	}
 
-	public static void selectRestaurantByCategory(String string) {
-		// TODO Auto-generated method stub
-		
+	public static void selectRestaurantByCategory(String category) {
+		try {
+			RunningEndView.printResult(RestaurantDAO.selectRestaurantByCategory(category));
+		} catch (SQLException e) {
+			System.out.println("selectRestaurantByCategory 검색 불가.");
+			e.printStackTrace();
+		}
 	}
 
-	public static void selectRestaurantByPrice(int i, int j) {
-		// TODO Auto-generated method stub
-		
+	public static void selectRestaurantByPrice(int low, int high) {
+		try {
+			RunningEndView.printResult(RestaurantDAO.selectRestaurantByPrice(low, high));
+		} catch (SQLException e) {
+			System.out.println("selectRestaurantByPrice 검색 불가.");
+			e.printStackTrace();
+		}
 	}
 
 	public static void selectRestaurantByDistance(int i) {
-		// TODO Auto-generated method stub
-		
+		try {
+			RunningEndView.printResult(RestaurantDAO.selectRestaurantByDistance(i));
+		} catch (SQLException e) {
+			System.out.println("selectRestaurantByRname 검색 불가.");
+			e.printStackTrace();
+		}
 	}
 
-	public static void selectRestaurantByRname(String string) {
-		// TODO Auto-generated method stub
-		
+	public static void selectRestaurantByRname(String rname) {
+		try {
+			RunningEndView.printResult(RestaurantDAO.selectRestaurantByRname(rname));
+		} catch (SQLException e) {
+			System.out.println("selectRestaurantByRname 검색 불가.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void selectRestaurantByPriceAndDistance(int low, int high, int i) {
+		try {
+			RunningEndView.printResult(RestaurantDAO.selectRestaurantByPriceAndDistance(low, high, i));
+		} catch (SQLException e) {
+			System.out.println("selectRestaurantByPriceAndDistance 검색 불가.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void selectRestaurantByCategoryAndPrice(String category, int low, int high) {
+		try {
+			RunningEndView.printResult(RestaurantDAO.selectRestaurantByCategoryAndPrice(category, low, high));
+		} catch (SQLException e) {
+			System.out.println("selectRestaurantByCategoryAndPrice 검색 불가.");
+			e.printStackTrace();
+		}
+	}
+
+	// ---- DML ----
+	public static void insertRestaurant(RestaurantDTO restaurant) {
+		try {
+			RunningEndView.printResult(RestaurantDAO.insertRestaurant(restaurant));
+		} catch (SQLException e) {
+			System.out.println("insertRestaurant 추가 불가.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void updateRestaurantByRname(String rname) {
+		try {
+			RunningEndView.printResult(RestaurantDAO.updateRestaurantByRname(rname));
+		} catch (SQLException e) {
+			System.out.println("updateRestaurantByRname 수정 불가.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void deleteRestaurantByRname(String rname) {
+		try {
+			RunningEndView.printResult(RestaurantDAO.deleteRestaurantByRname(rname));
+		} catch (SQLException e) {
+			System.out.println("deleteRestaurantByRname 삭제 불가.");
+			e.printStackTrace();
+		}
 	}
 
 }
